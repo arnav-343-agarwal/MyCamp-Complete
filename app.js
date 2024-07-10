13c505f598502e4881ddb2458b1683a4395822ec
Arnav Agarwal <agarwalarnav2003@gmail.com>
2024-07-10 09:00:13 +0530

    Section 59 Deployment

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 
-
 const mongoose = require('mongoose');
 const { config } = require('dotenv')
+const MongoDBStore = require('connect-mongo')(session)
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp3');
+// const dbUrl = process.env.DB_URL;
+const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp3'
+mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -44,7 +46,17 @@ app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname,'public')))
 app.use(mongoSanitize())
 
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret: 'thisshouldbeabettersecret',
+    touchAfter: 24*60*60
+})
+store.on("error",function(e){
+    console.log("SESSION STORE ERROR ",e);
+})
+
 const sessionConfig = {
+    store,
     name:'session',
     secret:"thisshouldbeabettersecret",
     resave: false,
